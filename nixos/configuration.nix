bf1d76bff427f0f163302b49591302a5ab7dc798
Preston Bennes <preston.bennes@gmail.com>
2016-03-20 13:53:23 -0700

    config updates

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,49 +1,56 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
 { config, pkgs, ... }:
 
 {
   imports =
-    [
+    [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
 
-  boot.kernelPackages = pkgs.linuxPackages_4_3;
-
-  networking.hostName = "nixos";
-
+  # Select internationalisation properties.
   i18n = {
     consoleFont = "Lat2-Terminus16";
     consoleKeyMap = "us";
     defaultLocale = "en_US.UTF-8";
   };
 
+  # Set your time zone.
   time.timeZone = "America/Los_Angeles";
 
-  # For the time being going to try managing packages under users.
-  # environment.systemPackages = with pkgs; [
-  #   wget
-  # ];
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    chromium
+    git
+    tmux
+    unzip
+    vim
+    zip
+  ];
 
-  services.openssh.enable = true;
+  # List services that you want to enable:
 
-  # If you want a desktop, uncomment.
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
   # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-  # services.xserver.layout = "us";
-  # Enable the KDE Desktop Environment.
-  # services.xserver.displayManager.kdm.enable = true;
-  # services.xserver.desktopManager.kde4.enable = true;
+  services.xserver.enable = true;
+  services.xserver.layout = "us";
 
+  # Enable the KDE Desktop Environment.
+  services.xserver.displayManager.kdm.enable = true;
+  services.xserver.desktopManager.kde4.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.extraUsers.preston = {
     isNormalUser = true;
-    home = "/home/preston";
+    uid = 1000;
     extraGroups = [ "wheel" ];
-    shell = "/run/current-system/sw/bin/bash";
-  };
-
-  security.sudo = {
-    wheelNeedsPassword = false;
   };
 
   # The NixOS release to be compatible with for stateful data such as databases.
-  system.stateVersion = "15.09";
+  system.stateVersion = "16.09";
 }
